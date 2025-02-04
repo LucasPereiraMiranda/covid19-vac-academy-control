f384a47eee50c3a2ec8fc693be56f4dba4691837
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-02-04 14:48:54 -0300

    refactor: change name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class='jambtrom'>Controle de Vacina</h1>
+                    <h1 class='jambtrom'>Controle de Vacinas</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
